93197a46ff6f86870d0f775809218f33463a8635
mestokaya1830 <mesto1830@outllok.com>
2025-07-16 20:45:14 +0200

    nnn

--- 0-basic/App.js
+++ 0-basic/App.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar, Image, ImageBackground } from 'react-native';
 import { Text, View, StyleSheet } from 'react-native';
+
+import img1 from './assets/img1.png'
+import imgBG from './assets/bg.jpg'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Merhaba Expo!</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar barStyle="dark-content" backgroundColor="red" />
+         <ImageBackground source={imgBG} style={styles.bgImgStyle}>
+          <View>
+            <Text style={styles.text}>Merhaba Expo!</Text>
+            <Image source={img1} style={styles.imgStyle} />
+          </View>
+         </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  bgImgStyle:{
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    alignItems:'center'
   },
+  text:{
+    textAlign:'center',
+    fontSize:30,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  imgStyle:{
+    width: 300,
+    height:200
+  }
 });
