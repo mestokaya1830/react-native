80342395f4adcdcccad35dd4a30c3647012f05f9
mestokaya1830 <mesto1830@outllok.com>
2025-07-17 23:14:30 +0200

    cc

--- 0-basic/App.js
+++ 0-basic/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { StatusBar, Image, ImageBackground } from 'react-native';
-import { Text, View, StyleSheet } from 'react-native';
+import { StatusBar, ScrollView, Text, View, StyleSheet, Image, ImageBackground } from 'react-native';
 
 import img1 from './assets/img1.png'
 import imgBG from './assets/bg.jpg'
@@ -10,13 +9,15 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar barStyle="dark-content" backgroundColor="red" />
-         <ImageBackground source={imgBG} style={styles.bgImgStyle}>
-          <View>
-            <Text style={styles.text}>Merhaba Expo!</Text>
-            <Image source={img1} style={styles.imgStyle} />
+          <StatusBar barStyle="dark-content" backgroundColor="red" />
+          <View style={{flex: 1}}>
+              <ImageBackground source={imgBG} style={styles.bgImgStyle}>
+                <ScrollView>
+                  <Text style={styles.text}>Merhaba Expo!</Text>
+                  <Image source={img1} style={styles.imgStyle} />
+                </ScrollView>
+              </ImageBackground>
           </View>
-         </ImageBackground>
       </SafeAreaView>
     </SafeAreaProvider>
   );
