309d066c4c73c83a44511014b77af74fb2328e26
mestokaya1830 <mesto1830@outllok.com>
2025-08-04 19:29:10 +0200

    vfvf

---
+++ click-events/App.js
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Button,
+  Alert,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+import img1 from "./assets/img1.jpg";
+
+export default function App(){
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar barStyle="light-content" backgroundColor={"green"} />
+      <View style={styles.container}>
+        <Text style={styles.text}>Content</Text>
+      </View>
+      <View>
+        <Text style={styles.textBtn} onPress={() => Alert.alert("Hello")}>
+          Click Me
+        </Text>
+      </View>
+      <View>
+        <TouchableOpacity onPress={() => Alert.alert("Hello")}>
+          <Text style={{textAlign: "center"}}>TouchableOpacity</Text>
+          <Image
+            source={img1}
+            style={styles.img}
+            onPress={() => Alert.alert("Hello")}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 30,
+  },
+  textBtn: {
+    alignSelf: "center",
+    width: 300,
+    backgroundColor: "red",
+    color: "white",
+    fontSize: 18,
+    textAlign: "center",
+    padding: 10,
+    borderRadius: 10,
+    padding: 10,
+    margin: 20
+  }
+  ,
+  img: {  
+    width: 300,
+    height: 100,
+    alignSelf: "center"
+  }
+});
