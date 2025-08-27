ba5a65c04054c77fa4a17ad63ab12cd208df44b9
mestokaya1830 <mesto1830@outllok.com>
2025-08-27 23:23:43 +0200

    njn

---
+++ expo-router-api/app/_layout.jsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return <Stack />;
+}
+
+
+//install
+//npx create-expo-app@latest  expo-router-api
+//then remove example project
+//npm run reset-project
