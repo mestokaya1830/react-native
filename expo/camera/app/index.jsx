174ab13a253632356d3b320398b2dc12d609e777
mestokaya1830 <mesto1830@outllok.com>
2025-09-02 17:56:21 +0200

    njn

---
+++ expo/camera/app/index.jsx
@@ -0,0 +1,25 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+      }}
+    >
+      <Text style={styles.text}>
+        <Link href="/camera">Camera</Link>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 30,
+  },
+});
